5f4ab02305c81f5a0cdec6d59951fa03d706e92a
Pedro Santos <donbonifacio@gmail.com>
2014-09-21 17:55:44 +0100

    Refactor unit tests

--- test/obb_rules/unit_test.clj
+++ test/obb_rules/unit_test.clj
@@ -3,6 +3,20 @@
 
 (deftest unit
   (testing "get-units"
-    (is (= 1 (count (obb-rules.unit/get-units))))))
+    (is (= 1 (count (get-units))))))
 
+(defn- test-unit
+  [name code]
+  (is (= name ((get-unit-by-name name) :name)))
+  (is (= code ((get-unit-by-name name) :code))))
+
+(defn- verify-unit
+  [unit]
+  (deftest unit-tester (test-unit (unit :name) (unit :code))))
+
+(defn- verify-units []
+  (doseq [unit (get-units)]
+    (verify-unit unit)))
+
+(verify-units)
 (run-tests)
