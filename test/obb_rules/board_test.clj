76a3b59fd52eec6c594ed2b0fa63e5eeb4c7f768
Pedro Santos <donbonifacio@gmail.com>
2014-09-24 22:29:55 +0100

    Add more info

--- test/obb_rules/board_test.clj
+++ test/obb_rules/board_test.clj
@@ -15,18 +15,20 @@
     (let [board (create-board 8 8)
           element :dummy-element-placeholder]
 
-      (let [new-board (place-element board [1 1] element)]
-        (is (= 1 (board-elements-count new-board)))
-        (let [fetched-element (get-element new-board [1 1])]
-          (is (= fetched-element element))
-          (is (= true (has-element? new-board [1 1])))
-          (is (= 1 (board-elements-count new-board)))))
+      (testing "operations with coordinate"
+        (let [new-board (place-element board [1 1] element)]
+          (is (= 1 (board-elements-count new-board)))
+          (let [fetched-element (get-element new-board [1 1])]
+            (is (= fetched-element element))
+            (is (= true (has-element? new-board [1 1])))
+            (is (= 1 (board-elements-count new-board))))))
 
-      (let [new-board (place-element board 1 1 element)]
-        (is (= 1 (board-elements-count new-board)))
-        (let [fetched-element (get-element new-board 1 1)]
-          (is (= fetched-element element))
-          (is (= true (has-element? new-board 1 1)))
-          (is (= 1 (board-elements-count new-board))))))))
+      (testing "operations with x y usage"
+        (let [new-board (place-element board 1 1 element)]
+          (is (= 1 (board-elements-count new-board)))
+          (let [fetched-element (get-element new-board 1 1)]
+            (is (= fetched-element element))
+            (is (= true (has-element? new-board 1 1)))
+            (is (= 1 (board-elements-count new-board)))))))))
 
 (run-tests)
