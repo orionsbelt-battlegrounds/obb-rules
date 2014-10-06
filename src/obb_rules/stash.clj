b5f074a9425dd62a235a0b87b08159fdeeaca2c0
Pedro Santos <donbonifacio@gmail.com>
2014-10-06 21:49:52 +0100

    Create stash

---
+++ src/obb_rules/stash.clj
@@ -0,0 +1,31 @@
+(ns obb-rules.stash)
+
+(defn create
+  "Creates a new stash"
+  [& units]
+  (apply hash-map units))
+
+(defn how-many?
+  "States how many of a unit are present"
+  [stash unit]
+  (or (stash unit) 0))
+
+(defn empty?
+  "Returns true if this stash is empty"
+  [stash]
+  (= 0 (count stash)))
+
+(defn available?
+  "Checks if a given quantity is available"
+  [stash unit quantity]
+  (<= quantity (how-many? stash unit)))
+
+(defn retrieve
+  "Removes units from stash"
+  [stash unit quantity]
+  (let [current-quantity (stash unit)
+        new-quantity (- current-quantity quantity)]
+    (assert (>= current-quantity quantity) "InvalidStashQuantity")
+    (if (= new-quantity 0)
+      (dissoc stash unit)
+      (assoc stash unit new-quantity))))
---
+++ test/obb_rules/stash_test.clj
@@ -0,0 +1,41 @@
+(ns obb-rules.stash-test
+  (:require [obb-rules.stash :as stash])
+  (:use clojure.test
+        midje.sweet
+        obb-rules.action
+        obb-rules.actions.move
+        obb-rules.board
+        obb-rules.element
+        obb-rules.result
+        obb-rules.unit))
+
+(deftest stash-creation
+  (testing "creates a stash given some units"
+    (is (stash/create :rain 20
+                      :crusader 10))))
+
+(deftest stash-access
+  (let [s (stash/create :anubis 20)]
+
+    (testing "is empty?"
+      (is (not (stash/empty? s))))
+
+    (testing "is available"
+      (is (stash/available? s :anubis 10)))
+
+    (testing "is not available"
+      (is (not (stash/available? s :anubis 100))))
+
+    (testing "unit count"
+      (is (= 20 (stash/how-many? s :anubis))))
+
+    (testing "get partial unit from stash"
+      (let [new-stash (stash/retrieve s :anubis 10)]
+        (is new-stash)
+        (is (= 10 (stash/how-many? new-stash :anubis)))))
+
+    (testing "get all unit from stash"
+      (let [new-stash (stash/retrieve s :anubis 20)]
+        (is new-stash)
+        (is (stash/empty? new-stash))
+        (is (= 0 (stash/how-many? new-stash :anubis)))))))
