2b26d4ce45cb97683ec3606e035c1d1f73a998cb
Pedro Santos <donbonifacio@gmail.com>
2014-10-12 22:03:18 +0100

    Refactor process-turn

--- src/obb_rules/turn.clj
+++ src/obb_rules/turn.clj
@@ -2,11 +2,15 @@
   (:require [obb-rules.action :as action]
             [obb-rules.result :as result]))
 
+(defn- apply-actions
+  "Applies the actions to a board"
+  [player current-game action]
+  (let [result (action current-game player)]
+    (result/result-board result)))
+
 (defn process
   "Processes the given actions"
   [game player & raw-actions]
-  (let [actions (map action/build-action raw-actions)]
-    (reduce (fn action-merger [current-game action]
-              (let [result (action current-game player)]
-                (result/result-board result)))
-            game actions)))
+  (let [actions (map action/build-action raw-actions)
+        do-actions (partial apply-actions player)]
+    (reduce do-actions game actions)))
