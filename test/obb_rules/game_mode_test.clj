8f68f7cfdf840ab88339f9246a57a753bf8f3da1
Pedro Santos <donbonifacio@gmail.com>
2014-10-18 16:07:24 +0100

    Add test

--- test/obb_rules/game_mode_test.clj
+++ test/obb_rules/game_mode_test.clj
@@ -1,6 +1,13 @@
 (ns obb-rules.game-mode-test
   (:require [obb-rules.game :as game]
             [obb-rules.board :as board]
+            [obb-rules.game-mode :as game-mode]
             [obb-rules.turn :as turn]
             [obb-rules.stash :as stash])
   (:use clojure.test))
+
+(deftest dont-finalize-on-deploy
+  (let [game-on-deploy (game/random)
+        final-game (game-mode/process game-on-deploy)]
+    (is (not (game-mode/final? game-on-deploy)))
+    (is (= :deploy (game/state final-game)))))
