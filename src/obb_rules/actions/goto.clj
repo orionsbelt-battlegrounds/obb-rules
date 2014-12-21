1bb3ff865b113c937261f0bab204f5be2dfeeabe
Pedro Santos <donbonifacio@gmail.com>
2014-12-21 12:27:00 +0000

    Default to :move when coords are adjacent

--- src/obb_rules/actions/goto.clj
+++ src/obb_rules/actions/goto.clj
@@ -1,9 +1,11 @@
 (ns obb-rules.actions.goto
   "Allows movement from a unit to non-adjacent coordinates.
   Translates itself to the best found path via :move actions."
-  (:require [obb-rules.actions.move :as move]))
+  (:require [obb-rules.actions.move :as move]
+            [obb-rules.board :as board]))
 
 (defn build-goto
   "Builds a goto action on a board"
-  [args]
-  (move/build-move args))
+  [[from to quantity]]
+  (if (board/adjacent? from to)
+    (move/build-move [from to quantity])))
