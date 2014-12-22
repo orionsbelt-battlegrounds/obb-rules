8660b74a9d8ed0d9d93daa62bed9d893270433a7
Pedro Santos <donbonifacio@gmail.com>
2014-12-22 09:51:30 +0000

    Fix typo

--- src/obb_rules/actions/auto_deploy.clj
+++ src/obb_rules/actions/auto_deploy.clj
@@ -1,5 +1,6 @@
 (ns obb-rules.actions.auto-deploy
-  "Auto deploys units to the battleground, based on predifined templates"
+  "Auto deploys units to the battleground, based on predefined templates"
+  ^{:added "1.5" :author "Pedro Santos"}
   (:require [obb-rules.result :as result]))
 
 (defn build-auto-deploy
