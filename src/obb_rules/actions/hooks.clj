c76c53a557b02aa75968d0faf3993335b29005e9
Pedro Santos <donbonifacio@gmail.com>
2014-12-28 12:48:39 +0000

    Start rebound

--- src/obb_rules/actions/hooks.clj
+++ src/obb_rules/actions/hooks.clj
@@ -2,12 +2,23 @@
       :author "Pedro Santos"}
   obb-rules.actions.hooks
   "Hooks on the main actions"
-  (:require [obb-rules.element :as element]))
+  (:require [obb-rules.element :as element]
+            [obb-rules.actions.rebound :as rebound]))
+
+(def hook-handlers {:rebound rebound/process})
+
+(defn- get-handler
+  "Gets a handler for a hook"
+  [hook-data]
+  (let [handler-name (first hook-data)
+        handler (get hook-handlers handler-name)]
+    (assert handler (str "No hook handler for " handler-name))
+    handler))
 
 (defn process
   "Processes the given hooks"
   [event {attacker :attacker board :board info :info :as args}]
   (if-let [hooks (element/hooks attacker event)]
-    [board info]
+    ((get-handler (first hooks)) (first hooks) args)
     [board info]))
 
---
+++ src/obb_rules/actions/rebound.clj
@@ -0,0 +1,11 @@
+(ns ^{:added "1.7"
+      :author "Pedro Santos"}
+  obb-rules.actions.rebound
+  "Processes rebound logic"
+  (:require [obb-rules.element :as element]))
+
+(defn process
+  "Processes the rebound for the given data"
+  [config {attacker :attacker board :board info :info :as args}]
+  [board info])
+
