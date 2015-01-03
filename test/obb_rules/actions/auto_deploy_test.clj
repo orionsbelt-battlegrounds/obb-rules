746decae88da628407b7384de39f377498509d86
Pedro Santos <donbonifacio@gmail.com>
2015-01-03 13:43:03 +0000

    Refactor tests

--- test/obb_rules/actions/auto_deploy_test.clj
+++ test/obb_rules/actions/auto_deploy_test.clj
@@ -52,38 +52,29 @@
     (is (stash/cleared? (board/get-stash (result/result-board result-p1) :p2)))
     (is (stash/cleared? (board/get-stash (result/result-board result-p1) :p1)))))
 
-(deftest test-check-fail-1
-  (let [stash (stash/create "crusader" 47 "nova" 47 "heavy-seeker" 47)
-        board (game/create stash)
-        result-p2 (process-action board :p2 [:auto-deploy :firingsquad])
+(defn- auto-deploy-for
+  "Auto deploys for both players for the given stash"
+  [stash deploy-template]
+  (let [board (game/create stash)
+        result-p2 (process-action board :p2 [:auto-deploy deploy-template])
         board2 (result/result-board result-p2)
-        result-p1 (process-action board2 :p1 [:auto-deploy :firingsquad])]
+        result-p1 (process-action board2 :p1 [:auto-deploy deploy-template])]
     (is (result/succeeded? result-p1))
     (is (result/succeeded? result-p2))
     (is (stash/cleared? (board/get-stash (result/result-board result-p1) :p2)))
     (is (stash/cleared? (board/get-stash (result/result-board result-p1) :p1)))))
 
+(deftest test-check-fail-1
+  (let [stash (stash/create "crusader" 47 "nova" 47 "heavy-seeker" 47)]
+    (auto-deploy-for stash :firingsquad)))
+
 (deftest test-check-fail-2
-  (let [stash (stash/create "nova" 1 "heavy-seeker" 1 "crusader" 1 "boozer" 1)
-        board (game/create stash)
-        result-p2 (process-action board :p2 [:auto-deploy :firingsquad])
-        board2 (result/result-board result-p2)
-        result-p1 (process-action board2 :p1 [:auto-deploy :firingsquad])]
-    (is (result/succeeded? result-p1))
-    (is (result/succeeded? result-p2))
-    (is (stash/cleared? (board/get-stash (result/result-board result-p1) :p2)))
-    (is (stash/cleared? (board/get-stash (result/result-board result-p1) :p1)))))
+  (let [stash (stash/create "nova" 1 "heavy-seeker" 1 "crusader" 1 "boozer" 1)]
+    (auto-deploy-for stash :firingsquad)))
 
 (defspec any-random-game-should-be-firing-squad-deployed
   (* obb-gen/scenarions-to-test 1)
   (prop/for-all [raw-stash (obb-gen/stash)]
-    (let [stash (stash/create-from-hash (apply hash-map (flatten raw-stash)))
-          board (game/create stash)
-          result-p2 (turn/process board :p2 [:auto-deploy :firingsquad])
-          board2 (result/result-board result-p2)
-          result-p1 (turn/process board2 :p1 [:auto-deploy :firingsquad])]
-      (is (result/succeeded? result-p1))
-      (is (result/succeeded? result-p2))
-      (is (stash/cleared? (board/get-stash (result/result-board result-p1) :p2)))
-      (is (stash/cleared? (board/get-stash (result/result-board result-p1) :p1))))))
+    (let [stash (stash/create-from-hash (apply hash-map (flatten raw-stash)))]
+      (auto-deploy-for stash :firingsquad))))
 
