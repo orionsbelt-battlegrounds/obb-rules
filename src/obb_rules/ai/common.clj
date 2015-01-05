be6208abba19f21123d866f89fcfe3e2166451d9
Pedro Santos <donbonifacio@gmail.com>
2015-01-05 21:21:13 +0000

    Find target for direct hit

--- src/obb_rules/ai/common.clj
+++ src/obb_rules/ai/common.clj
@@ -3,12 +3,39 @@
   "Common utilities for ai bots"
   (:require [obb-rules.game :as game]
             [obb-rules.stash :as stash]
+            [obb-rules.turn :as turn]
+            [obb-rules.result :as result]
+            [obb-rules.actions.direction :as dir]
             [obb-rules.element :as element]
             [obb-rules.board :as board]
             [obb-rules.unit :as unit]))
+
+(defn- process
+  "Processes an action on a game/board"
+  [board element raw-action]
+  (let [player (element/element-player element)]
+    (turn/process-actions board player [raw-action])))
+
+(defn- build-target
+  "Builds a result that represents a successful target"
+  [result raw-action])
+
+(defn- find-targets
+  "Finds possible attack targets for the given element"
+  [game attacker targets current-coordinate distance]
+  (let [unit (element/element-unit attacker)
+        direction (element/element-direction attacker)
+        next-coordinate (dir/update direction current-coordinate)
+        origin (element/element-coordinate attacker)
+        next-element (board/get-element game next-coordinate)
+        raw-action [:attack origin next-coordinate]
+        result (process game attacker raw-action)]
+    (if (result/failed? result)
+      targets
+      (conj targets (build-target result raw-action)))))
 
 (defn attack-options
   "Returns a collection of possible options for attack
   for the given element"
   [game element]
-  [])
+  (find-targets game element [] (element/element-coordinate element) 0))
--- test/obb_rules/ai/common_test.clj
+++ test/obb_rules/ai/common_test.clj
@@ -21,3 +21,10 @@
                   (place-element [2 5] (create-element :p1 rain 1 :north [2 5])))
         options (common/attack-options board (board/get-element board [2 5]))]
     (is (= 0 (count options)))))
+
+(deftest get-direct-attack-options
+  (let [board (-> (board/create-board)
+                  (place-element [2 5] (create-element :p1 rain 1 :south [2 5]))
+                  (place-element [2 6] (create-element :p2 rain 1 :north [2 6])))
+        options (common/attack-options board (board/get-element board [2 5]))]
+    (is (= 1 (count options)))))
