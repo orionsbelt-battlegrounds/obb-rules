9b97f306bd3c838537d8a49d1121da6ad02f7278
Pedro Santos <donbonifacio@gmail.com>
2015-06-20 16:59:10 +0100

    Update deps and support only java8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject obb-rules "1.12.0"
+(defproject obb-rules "1.13.0"
   :description "Orion's Belt battle rules"
   :url "https://github.com/orionsbelt-battlegrounds/obb-rules"
 
@@ -7,7 +7,7 @@
             :distribution :repo
             :comments     "Copyright 2011-2014 Pedro Santos All Rights Reserved."}
 
-  :dependencies [[org.clojure/clojure "1.7.0-alpha6"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
 
   :source-paths ["src"]
@@ -16,20 +16,14 @@
   :scm {:name "git"
         :url "git@github.com:orionsbelt-battlegrounds/obb-rules.git"}
 
-  :aliases {"all" ["with-profile" "dev:1.6:1.7" "test"]}
-
   :profiles {:production {
                    :global-vars {*warn-on-reflection* false
                                  *assert* false}}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]
-                                  [org.clojure/test.check "0.6.2"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha6"]
-                                  [org.clojure/test.check "0.6.2"]]}
-             :dev {:dependencies [[org.clojure/test.check "0.6.2"]
+             :dev {:dependencies [[org.clojure/test.check "0.7.0"]
                                   [criterium "0.4.3"]]
                    :global-vars {*warn-on-reflection* false
                                  *assert* true}
-                   :plugins [[com.jakemccrary/lein-test-refresh "0.5.5"]
+                   :plugins [[com.jakemccrary/lein-test-refresh "0.9.0"]
                              [lein-cloverage "1.0.2"]]}}
 
   )
