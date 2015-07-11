2ead38193705bf7e29077b5045a5d6a4a5e47794
Pedro Santos <donbonifacio@gmail.com>
2015-07-11 19:20:11 +0100

    Prepare proper game data

--- src/obb_demo/views/play.cljs
+++ src/obb_demo/views/play.cljs
@@ -5,19 +5,31 @@
             [obb-rules.result :as result]
             [obb-demo.boardground :as boardground]))
 
+(defn- deployed-game
+  "Creates a deployed game"
+  []
+
+  (-> (game/random)
+      (turn/process-actions :p1 [[:auto-deploy :firingsquad]])
+      (result/result-board)
+      (turn/process-actions :p2 [[:auto-deploy :firingsquad]])
+      (result/result-board)))
+
+(defn- get-game-data
+  "Gets the current game or creates a new one"
+  [state]
+  (if-let [game (:index state)]
+    game
+    (let [game (deployed-game)
+          game-data {:game game}]
+      (state/set-page-data! game-data))))
+
 (defn render
   [state]
-  (let [game (game/random)
-        result (turn/process-actions game :p1 [[:auto-deploy :firingsquad]])
-        game2 (result/result-board result)
-        result2 (turn/process-actions game2 :p2 [[:auto-deploy :firingsquad]])
-        game2 (result/result-board result2)]
+  (let [game-data (get-game-data state)
+        game (:game game-data)]
     [:div.row
       [:div.col-lg-5
-        [boardground/render {} game2]]
-      [:div.col-lg-3
-        [boardground/render {} game2]]
+        [boardground/render {} game]]
       [:div.col-lg-2
-        [boardground/render {} game2]]
-      [:div.col-lg-1
-        [boardground/render {} game2]]]))
+        [boardground/render {} game]]]))
